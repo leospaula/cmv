3130dd76fa1f81f238595eef1415ffd4162d7487
Leonardo <leo.spaula@gmail.com>
2015-07-02 14:27:45 -0300

    Add file upload

--- app/models/transparencia.rb
+++ app/models/transparencia.rb
@@ -1,4 +1,5 @@
 class Transparencia < ActiveRecord::Base
+	mount_uploader :arquivo, ArquivoUploader
 	def self.anos
   		anos = %w[2010 2011 2012 2013 2014 2015 2016 2017 2018]
   	end
--- app/uploaders/arquivo_uploader.rb
+++ app/uploaders/arquivo_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class ImagemUploader < CarrierWave::Uploader::Base
+class ArquivoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
@@ -8,12 +8,12 @@ class ImagemUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-    storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}+#{model.nome.to_s.underscore}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}+#{model.ano}+#{model.mes}+#{model.tipo.to_s.underscore}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -32,9 +32,9 @@ class ImagemUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [200, 200]
-  end
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
